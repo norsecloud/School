4b2798bd78486693ecf7a81085859607b9246e4f
Norsecloud <36419990+norsecloud@users.noreply.github.com>
2020-10-08 16:47:04 -0400

    Assignment 1 OS
    multithreaded producer consumer
    c command interpreter

---
+++ Operating Systems/Assignment 1/ProducerConsumer.java
@@ -0,0 +1,110 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+//Finn Raae
+// 9/9/2020
+//Operating Systems - Assignment 1
+//Write a program in Java which creates producer and consumer threads. The
+//producer should “produce” by setting the elements of an array of integers to
+//FULL. The consumer should “consume” by setting the elements of an array of
+//integers to EMPTY.
+
+public class ProducerConsumer {
+
+	public static void main(String[] args) {
+		//variables
+		List<Integer> array = new ArrayList<Integer>();
+		Thread prodThread = new Thread(new Producer(array));
+		Thread consThread = new Thread(new Consumer(array));
+		
+		//run threads
+		prodThread.start();
+		consThread.start();
+	}
+
+}
+
+class Producer implements Runnable{//produces numbers to fill array
+	//variables
+	List<Integer> array = null;		//array that will be filled and emptied
+	final int SIZE = 10;			//size of array
+	private int i = 1; 				//add variable
+	
+	//constructor	
+	public Producer(List<Integer> Array) {
+		super();
+		this.array = Array;
+	}
+
+	@Override
+	public void run() {
+		while(true) {
+			try {
+				produce(i++);			//call produce
+			}catch(InterruptedException exception) {
+				
+			}
+		}	
+	}
+	
+	public void produce(int x) throws InterruptedException {
+		
+		synchronized (array) {
+			while(array.size() == SIZE){ //while full
+				System.out.println(Arrays.toString(array.toArray()));
+				System.out.println("Array is Full: Start Consume");
+				array.wait();
+			}
+		}
+		
+		synchronized (array) { //when not full
+			System.out.println("Produced the element: " + x);
+			array.add(x);
+			Thread.sleep(500);		//wait so its not super fast
+			array.notify();
+		}
+		
+	}
+}
+class Consumer implements Runnable{//produces numbers to fill array
+	//variables
+//	int[] arr = null;
+	List<Integer> array = null;		//array that will be filled and emptied
+
+	//constructor
+	public Consumer(List<Integer> Array) {
+		super();
+		this.array = Array;
+	}
+
+	@Override
+	public void run() {
+		while(true) {
+			try {
+				consume();			//call consume
+			}catch(InterruptedException exception) {
+				
+			}
+		}
+		
+	}
+	
+	public void consume() throws InterruptedException {
+		
+		synchronized (array) {
+			while(array.isEmpty()){ //while empty
+				System.out.println(Arrays.toString(array.toArray()));
+				System.out.println("Array is Empty: Start Produce");
+				array.wait();
+			}
+		}
+		
+		synchronized (array) { //when not full
+			Thread.sleep(500);
+			System.out.println("Consume the element: " + array.remove(0));
+			array.notify();
+		}
+		
+	}
+}
---
+++ Operating Systems/Assignment 1/main.c
@@ -0,0 +1,84 @@
+//Finn Raae
+//9/11/2020
+//Operating Systems - Assignment 1: command interpreter
+//Write a program in C which functions as a DOS command interpreter. DOS uses
+//the commands cd, dir, type, del, ren, and copy to do the same functions as the
+// UNIX commands cd, ls, cat, rm, mv, and cp. Your program loops continuously,
+// allowing the user to type in DOS commands, which are stored in the variables
+// command, arg1 and arg2. The command should be considered by a case
+// statement, which executes an appropriate UNIX command, depending on which
+// DOS command has been given. The program should echo the following
+// instruction to the user: Type Ctrl-C to exit.
+
+#include <stdio.h>
+#include <string.h>
+
+
+int main(void) {
+  //variables
+  char command[100];
+  char arg1[100];
+  char arg2[100];
+
+  //infinite loop
+  while(1) {   
+      printf("Type Ctrl-C to exit.\nInput the DOS Command: ");   //ask for input for command
+      scanf("%s", command);
+  
+      // if statements
+      if(strcmp(command, "cd") == 0){
+        printf("Enter the argument to cd command: ");
+        scanf("%s", arg1);
+        printf("\nUNIX Command: cd %s\n\n", arg1);
+        chdir(arg1);
+      }
+      else if(strcmp(command, "dir") == 0){
+        printf("\nUNIX Command: ls\n\n");
+        system("ls");
+      }
+      else if(strcmp(command, "type") == 0){
+        printf("Enter the argument to type command: ");
+        scanf("%s", arg1);
+        char str1[20] = "cat ";
+        strcat(str1, arg1);
+        printf("\nUNIX Command: %s\n\n", str1);
+        system(str1);
+      }
+      else if(strcmp(command, "del") == 0){
+        printf("Enter the argument to del command: ");
+        scanf("%s", arg1);
+        char str1[20] = "rm ";
+        strcat(str1, arg1);
+        printf("\nUNIX Command: %s\n\n", str1);
+        system(str1);
+      }
+      else if(strcmp(command, "ren") == 0){
+        printf("Enter the first argument to ren command: ");
+        scanf("%s", arg1);
+        printf("Enter the second argument to ren command: ");
+        scanf("%s", arg2);
+        char str1[20] = "mv ";
+        strcat(str1, arg1);
+        strcat(str1, " ");
+        strcat(str1, arg2);
+        printf("\nUNIX Command: %s\n\n", str1);
+        system(str1);
+      }
+      else if(strcmp(command, "copy") == 0){
+        printf("Enter the first argument to copy command: ");
+        scanf("%s", arg1);
+        printf("Enter the second argument to copy command: ");
+        scanf("%s", arg2);
+        char str1[20] = "cp ";
+        strcat(str1, arg1);
+        strcat(str1, " ");
+        strcat(str1, arg2);
+        printf("\nUNIX Command: %s\n\n", str1);
+        system(str1);
+      }
+      else{//if incorrenct command
+        printf("\nInvalid entry please use a DOS command.\nPlease enter cd, dir, type, del, ren, or copy.\n\n");
+      }//end if statements
+   }//end while
+  return 0;
+}//end prog
